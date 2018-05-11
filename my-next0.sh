d88dd4570c682a3bff3a1a4f898d4653ca4c5585
Russell Cattelan <cattelan@digitalelves.com>
2018-05-11 22:40:21 +0000

    Add the docker build and start scripts

---
+++ my-next0.sh
@@ -0,0 +1,14 @@
+docker run --rm --tty -it\
+  --cap-add LINUX_IMMUTABLE \
+  --hostname $USER-next0 \
+  --name $USER-next0 \
+  -e DISPLAY=$DISPLAY \
+  -p 11022:22 \
+  --ipc=host \
+  --volume /LOSRLPGD03/development:/LOSRLPGD03/development:rw \
+  --volume /LOSRLPGD03/data2:/LOSRLPGD03/data2:rw \
+  --volume /home/ipecm:/home/ipecm:ro \
+  --volume /home/rcattelan:/home/rcattelan:rw \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  --workdir /home/espa \
+  epsa/ips-next0 bash
