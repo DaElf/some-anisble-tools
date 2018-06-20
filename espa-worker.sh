d3f2e7bec85cfaff9656241b3f98bc5d5d17ce40
Russell Cattelan <cattelan@digitalelves.com>
2018-06-20 19:29:40 +0000

    Remove vars that will come in via the docker container

--- espa-worker.sh
+++ espa-worker.sh
@@ -6,9 +6,7 @@ pid=$!; echo "simple server pid $pid"
 popd
 
 gpg --import USGS_private.gpg || true
-export ESPA_CONFIG_PATH=$(pwd)
-export SQSBatchQueue=mvp2-espa-ecs-batch-SQSBatchQueue-1WKH30SU1TQN3.fifo
-export AWS_DEFAULT_REGION=us-west-2
+
 espa-worker
 
 kill $pid
