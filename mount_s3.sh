ce24d549164d1357ead19385c2784fc3c8e5d321
Russell Cattelan <cattelan@digitalelves.com>
2018-06-28 22:52:36 +0000

    Test script for goofys mount

---
+++ mount_s3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -x
+
+#. ./dofoo.sh
+mkdir -p /s3/auxiliaries/elevation
+./goofys -o allow_other lsds-mvp-elevation-aux /s3/auxiliaries/elevation
+mkdir -p /s3/auxiliaries/aster
+./goofys -o allow_other lsds-mvp-aster-aux /s3/auxiliaries/aster
+mkdir -p /s3/auxiliaries/L47
+./goofys -o allow_other lsds-mvp-l47-sr-aux /s3/auxiliaries/L47
+mkdir -p /s3/auxiliaries/L8
+./goofys -o allow_other lsds-mvp-l8-sr-aux /s3/auxiliaries/L8
+mkdir -p /s3/auxiliaries/NARR
+./goofys -o allow_other lsds-mvp-narr-aux:/NARR /s3/auxiliaries/NARR
+
