01ca877cc6eeac30af0946a73f1ccd37b18e296f
Russell Cattelan <cattelan@thebarn.com>
2018-07-02 14:20:53 -0500

    Better description

--- ec2_tools/create_ami.sh
+++ ec2_tools/create_ami.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 
 my_id=$(cat image_id)
-aws ec2 create-image --instance-id $my_id --name "daelf-compute-04" --description "AMI for ESPA compute processing"
+aws ec2 create-image --instance-id $my_id --name "ESPA_ProcessComputeStorageS3" --description "AMI for ESPA compute processing"
