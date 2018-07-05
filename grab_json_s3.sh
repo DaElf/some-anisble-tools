6f99bf0628b4885bf8e2f31088a7310ad010e6d2
Russell Cattelan <cattelan@digitalelves.com>
2018-07-05 22:02:15 +0000

    ESPA_CONFIG_PATH is no longer read by the worker

--- grab_json_s3.sh
+++ grab_json_s3.sh
@@ -6,15 +6,15 @@ sudo chmod 777 /jobtmp
 my_dir=$(dirname "$0")
 cd $my_dir
 
+exit 1
+
 sudo -u espa -s <<EOF
 set -x
 pwd
 
 gpg --import ./gpg-tools/USGS_private.gpg || true
 
-export ESPA_CONFIG_PATH=$my_dir
 export AWS_DEFAULT_REGION=us-west-2
-
 export PYTHONPATH=/devel/daelf/espa-all/espa-processing/processing
 
 time /devel/daelf/espa-all/espa-processing/processing/espa_batch_worker.py $1
