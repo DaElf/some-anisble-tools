b4bbf7233473d64ad79004fc8682412b9cf8ae55
Russell Cattelan <cattelan@digitalelves.com>
2018-07-06 01:14:00 +0000

    Clean up and move files around
    This repo is still a bit of junk box of tools that will be
    need more organization.

--- espa-worker.sh
+++ espa-worker.sh
@@ -1,12 +1,26 @@
 #!/bin/sh -x
 
-pushd /efs/auxiliaries/aster
-python -m SimpleHTTPServer 1666 &
-pid=$!; echo "simple server pid $pid"
-popd
+#sudo mkdir -p /media/ephemeral0
+#sudo chmod 777 /media/ephemeral0
+sudo chmod 777 /jobtmp
+my_dir=$(dirname "$0")
+cd $my_dir
 
-gpg --import USGS_private.gpg || true
+exit 1
 
-espa-worker
+sudo -u espa -s <<EOF
+set -x
+pwd
 
-kill $pid
+gpg --import ./gpg-tools/USGS_private.gpg || true
+
+export AWS_DEFAULT_REGION=us-west-2
+export PYTHONPATH=/devel/daelf/espa-all/espa-processing/processing
+
+time /devel/daelf/espa-all/espa-processing/processing/espa_batch_worker.py $1
+
+EOF
+
+ret=$?
+echo "Exit now" $ret
+exit $ret
