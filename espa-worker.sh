18bfbfcafa78d3680e62e2d82d90fb5b1d204047
Russell Cattelan <cattelan@digitalelves.com>
2018-07-06 21:52:35 +0000

    Use the version of espa-batch-worker that is installed in container

--- espa-worker.sh
+++ espa-worker.sh
@@ -1,21 +1,15 @@
 #!/bin/sh -x
 
-#sudo mkdir -p /media/ephemeral0
-#sudo chmod 777 /media/ephemeral0
 sudo chmod 777 /jobtmp
 my_dir=$(dirname "$0")
 cd $my_dir
 
-sudo -u espa -s <<EOF
+sudo -E -u espa -s <<EOF
 set -x
-pwd
+env
 
 gpg --import ./gpg-tools/USGS_private.gpg || true
-
-export AWS_DEFAULT_REGION=us-west-2
-export PYTHONPATH=/devel/daelf/espa-all/espa-processing/processing
-
-time /devel/daelf/espa-all/espa-processing/processing/espa_batch_worker.py $1
+(cd run_data; time espa-batch-worker $1)
 
 EOF
 
