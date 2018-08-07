a81ae4e7aa3eb64f5facb8503bd973d924e663f7
Dan Tonkin <dtonkin@contractor.usgs.gov>
2018-08-07 19:39:03 +0000

    Added run commands for Jenkins automation integration

---
+++ SQS/run_jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+# Usage:
+# 1) Set the environment variables below to match your environment/directories
+# 2) run_multi.sh ###, where ### is the number of scenes to process
+# 3) Check your batch status and/or the L2 output S3 bucket for results
+
+SQSHOME=.
+export espaQueue=test-vpc-Batch-ESPA_ProcessJobQueueSpot
+export espaJobBucket=jdc-test-dev
+export espaJobDefinition=test-vpc-Batch-ESPA_ProcessJob
+export AWSRegion=us-west-2
+export ESPA_PROCESS_TEMPLATE=$SQSHOME/espa-processing/processing/order_template.json
+export ESPA_CONFIG_PATH=$SQSHOME/espa-container-tools/
+
+export PYTHONPATH=i$SQSHOME/espa-container-tools/SQS:$SQSHOME/espa-processing/processing
+export PATH=$PATH:$PYTHONPATH
+
+$SQSHOME/espa-container-tools/SQS/submit_array.py \
+	--input-job-file ./jenkins_scenelist.txt \
+        --full \
+        $1
+
