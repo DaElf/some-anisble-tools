0d0ec6348bf5a03b908920874f3007cee33ce287
Dan Tonkin <dtonkin@contractor.usgs.gov>
2018-09-04 21:44:01 +0000

    Added initial run_multi based on current state of testing.
    You will need to update the script to reflect your Batch env.
    
    Currently will only accept from 2-15 scenes as an argument.

---
+++ ips-local-testing/run_multi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash +x
+
+export IPS_ALL_DIR=/devel/djt/ips-all
+export IPS_QUEUE=djt-batch-IPS_ProcessJobQueueSpot
+export IPS_JOBDEF=djt-batch-IPS_ProcessJob
+
+$IPS_ALL_DIR/espa-container-tools/ips-local-testing/ips_submit_multi.py \
+               --queue $IPS_QUEUE \
+               --job-definition $IPS_JOBDEF \
+               --input-bucket dev-lsds-l8-test-l0rp \
+               --job-bucket jdc-test-dev \
+               --batch-command $IPS_ALL_DIR/ips/ips-batch-worker.sh \
+               $1 
