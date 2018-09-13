aa62e1ee27c8b615686bfc82d9d64d6e2f5a3d3d
Russell Cattelan <cattelan@digitalelves.com>
2018-09-13 20:07:23 +0000

    ips_submit_scene updates

--- ips-local-testing/ips_submit_scene.sh
+++ ips-local-testing/ips_submit_scene.sh
@@ -3,11 +3,15 @@
 dir=$(dirname "$0")
 . $dir/env-aws.sh
 
-#my_dir=$(mktemp -p /jobtmp -d -t ips-worker.XXXXXX) || exit 1
+my_dir=$(mktemp -p /jobtmp -d -t ips_worker.XXXXXX) || exit 1
+chmod 777 $my_dir
 log=/jobtmp/log
 mkdir -p $log
 export IAS_LOG=$log
 env
+
+mkdir -p $my_dir/LC80170392013141LGN03
+aws s3 cp s3://dev-lsds-l8-test-l0rp/LC80170392013141LGN03.tar.gz - | tar --use-compress-program=pigz -xvf - -C $my_dir/LC80170392013141LGN03
 
 string=$(/opt/bin/PWG \
     -parm DSW:CAL_PARM_FILENAME=/s3/auxiliaries/landsat/cpf/LC08CPF_20130401_20130627_02.01 \
@@ -16,13 +20,19 @@ string=$(/opt/bin/PWG \
     -parm DSW:RLUT_FILENAME=/s3/auxiliaries/landsat/rlut/LC08RLUT_20130211_20150302_01_11.h5 \
     -parm DFP:L1G_PACKAGE=1 \
     -parm DFP:NAME_USING_PRODUCT_ID=1 \
+    -script DSC:SKIP \
     -scene LC80170392013141LGN03  \
     -procedure "L1T with Quality Band" \
-    -l0r_data_path /s3/l0rp/LC80170392013141LGN03/ \
+    -l0r_data_path $my_dir/LC80170392013141LGN03/ \
     -i)
 
+#    -parm  DSW:CREATE_RADIOMETRIC_CHAR_FLAG=0
+
 job=$(echo $string | awk -F',|( +)' '{print $11}')
-time /opt/bin/PWC $job
+/bin/time -o $my_dir/run_time -v /devel/daelf/ips-all/ips/ias_base/pcs/PWC $job
 ret=$?
+cat /jobtmp/$job/save/$job.log
+
+cat $my_dir/run_time
 echo "Exit now" $ret
 exit $ret
