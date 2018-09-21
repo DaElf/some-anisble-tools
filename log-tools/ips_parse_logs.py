932718ed5c5bdd313facfb8970a80d1f191db1bf
John Cavanaugh <jcavanaugh@contractor.usgs.gov>
2018-09-21 16:44:41 +0000

    Initial commit of ips_parse_logs.py
    Commit a basic version of an IPS CloudWath log parser.  It will
    scan the batch logs (created by the execution of ips-batch-worker)
    and find and categorize a few errors.

---
+++ log-tools/ips_parse_logs.py
@@ -0,0 +1,202 @@
+#! /usr/bin/env python
+
+
+import sys
+import boto3
+import datetime
+import dateutil.parser
+import calendar
+import argparse
+
+
+def parse_command_line():
+    """Parse the command line arguments
+    """
+
+    parser = argparse.ArgumentParser(
+            description = "Search AWS logs for IPS processing errors")
+    parser.add_argument('--start-time',
+            type = str,
+            dest = 'start_time',
+            action = 'store',
+            default = None,
+            help = "Start time for search")
+    parser.add_argument('--end-time',
+            type = str,
+            dest = 'end_time',
+            action = 'store',
+            default = None,
+            help = "End time for search")
+
+    args = parser.parse_args()
+
+    return args
+
+
+def classify_failure(message):
+    """Look for a reason for a failure by looking for a string
+    in the message.  Return a string with the reason, or None
+    if we don't know what it was.
+    """
+
+    failure_types = [
+            ("Missing OLI BPF file", "Error: can't find BPF_OLI"),
+            ("Missing TIRS BPF file", "Error: can't find BPF_TIRS"),
+            ("Missing CPF file", "Error: can't find CPF"),
+            ("Missing RLUT file", "Error: can't find RLUT"),
+            ("Error exit", "exit 1"),
+            ("Exception", "Exception")
+    ]
+
+    for (reason, err_string) in failure_types:
+        if message.find(err_string) != -1:
+            return reason
+
+    return None
+
+
+def search_log(client, group, log_stream, startTime=None, endTime=None):
+    """Search a log stream for an occurrence of the given error string.
+    Print a message for each error we find.
+    """
+
+    done = False
+    next_token = ''
+    exit_code = 0
+    stream = log_stream['logStreamName']
+
+    err_strings = ['Error:']
+    exit_string = '+ exit '
+
+    while not done:
+        if next_token != '':
+            events = client.get_log_events(
+                    logGroupName=group,
+                    logStreamName=stream,
+                    nextToken=next_token)
+        else:
+            events = client.get_log_events(
+                    logGroupName=group,
+                    logStreamName=stream)
+
+        for event in events['events']:
+            msg = event['message']
+            for err_string in err_strings:
+                if msg.find(err_string) != -1:
+                    reason = classify_failure(msg)
+                    if reason is not None:
+                        print("Error detected ({}):".format(reason))
+                    else:
+                        print("Error detected:")
+                    print("    " + stream)
+                    return 1
+
+                if msg.find(exit_string) != -1:
+                    exit_code = int(msg.split(' ')[-1])
+                    if exit_code != 0:
+                        print("Error exit code {}:".format(exit_code))
+                        print("    " + stream)
+                    return exit_code
+
+        if 'nextToken' in events:
+            next_token = events['nextToken']
+        else:
+            done = True
+
+        print("Error detected (job did not complete):")
+        print("    " + stream)
+        return 1
+
+    return exit_code
+
+
+def search_log_streams(group, prefix, startTime=None, endTime=None):
+    """Search the streams in a log group for occurrences of
+    the given error string.
+    """
+
+    done = False
+    stream_count = 0
+    error_exit_count = 0
+    next_token = ''
+    client = boto3.client('logs')
+    while not done:
+        if next_token != '':
+            streams = client.describe_log_streams(
+                    logGroupName=group,
+                    logStreamNamePrefix=prefix,
+                    nextToken=next_token)
+        else:
+            streams = client.describe_log_streams(
+                    logGroupName=group,
+                    logStreamNamePrefix=prefix)
+
+        for log_stream in streams['logStreams']:
+
+            # Check for empty log file
+            try:
+                stream = log_stream['logStreamName']
+                log_start_time = log_stream['firstEventTimestamp']
+                log_end_time = log_stream['lastEventTimestamp']
+            except KeyError:
+                continue
+
+            if startTime is not None and log_end_time < startTime:
+                continue
+            if endTime is not None and log_start_time > endTime:
+                continue
+            stream_count += 1
+            exit_code = search_log(client, group, log_stream)
+            if exit_code != 0:
+                error_exit_count += 1
+
+        if 'nextToken' in streams:
+            next_token = streams['nextToken']
+        else:
+            done = True
+
+    print("{} log streams found, {} with error(s)".format(
+            stream_count, error_exit_count))
+
+
+def main():
+    """Scan AWS logs for errors in IPS processing.  We look for
+    strings that we know are printed when errors occur.
+    """
+
+    args = parse_command_line()
+
+    start_timestamp = None
+    end_timestamp = None
+    if args.start_time is not None:
+        try:
+            start = dateutil.parser.parse(args.start_time)
+            ts = start.timetuple()
+            start_timestamp = calendar.timegm(ts) * 1000
+        except ValueError as e:
+            sys.stderr.write("Error: start time invalid ({})\n".format(e))
+            sys.exit(1)
+    if args.end_time is not None:
+        try:
+            end = dateutil.parser.parse(args.end_time)
+            ts = end.timetuple()
+            end_timestamp = calendar.timegm(ts) * 1000
+        except ValueError as e:
+            sys.stderr.write("Error: end time invalid ({})\n".format(e))
+            sys.exit(1)
+    if start_timestamp is not None and end_timestamp is None:
+        # End time was not specified.  Set it to now.
+        end = datetime.datetime.utcnow()
+        ts = end.timetuple()
+        end_timestamp = calendar.timegm(ts) * 1000
+    if start_timestamp is not None and end_timestamp is not None and \
+            end_timestamp <= start_timestamp:
+        sys.stderr.write("Error: end time earlier than start time\n")
+        sys.exit(1)
+
+    search_log_streams('/aws/batch/job', 'ips-devel-batch-IPS_ProcessJob',
+            startTime=start_timestamp, endTime=end_timestamp)
+
+# Set this to false if you want to use --prefix
+if __name__ == '__main__':
+    main()
