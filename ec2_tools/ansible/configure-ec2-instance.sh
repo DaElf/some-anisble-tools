0a2f36bacbc237296f6335d6346b0decde5e89da
Russell Cattelan <cattelan@thebarn.com>
2018-10-02 10:50:42 -0500

    add the workdir to the docker run of ansible

--- ec2_tools/ansible/configure-ec2-instance.sh
+++ ec2_tools/ansible/configure-ec2-instance.sh
@@ -1,6 +1,9 @@
+#!/bin/sh
+
 docker run --rm \
     -v $(pwd):/ansible/playbooks \
     -v ${HOME}/.ssh:/root/.ssh \
     -v ${HOME}/.aws:/root/.aws ansiblecm \
+    --workdir $(pwd) \
     -i /ansible/playbooks/hosts \
     /ansible/playbooks/playbook.yml
--- ec2_tools/ansible/create-ec2-instance.sh
+++ ec2_tools/ansible/create-ec2-instance.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+
 docker run --rm \
     -v $(pwd):/ansible/playbooks \
     -v ${HOME}/.aws:/root/.aws \
+    --workdir $(pwd) \
     ansiblecm \
     /ansible/playbooks/playbook_ec2.yml
