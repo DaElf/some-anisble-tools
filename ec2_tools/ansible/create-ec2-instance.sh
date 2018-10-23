75e0899d14d20c0b54df684255f5f753c96c72b3
Ron Dilley <ronald.dilley.ctr@usgs.gov>
2018-10-23 10:52:07 -0500

    specified the ansible version

--- ec2_tools/ansible/create-ec2-instance.sh
+++ ec2_tools/ansible/create-ec2-instance.sh
@@ -4,5 +4,5 @@ docker run --rm \
     -v $(pwd):/ansible/playbooks \
     -v ${HOME}/.aws:/root/.aws \
     --workdir $(pwd) \
-    ansiblecm \
+    ansiblecm:2.6.4 \
     /ansible/playbooks/playbook_ec2.yml
