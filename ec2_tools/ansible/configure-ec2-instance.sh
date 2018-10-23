df1ed998ac3eba14cb38725b5c89ef7b8aceaf6e
Ron Dilley <ronald.dilley.ctr@usgs.gov>
2018-10-23 10:51:38 -0500

    added USER env var and re-arranged the command line, also specified the ansiblecm version

--- ec2_tools/ansible/configure-ec2-instance.sh
+++ ec2_tools/ansible/configure-ec2-instance.sh
@@ -3,7 +3,9 @@
 docker run --rm \
     -v $(pwd):/ansible/playbooks \
     -v ${HOME}/.ssh:/root/.ssh \
-    -v ${HOME}/.aws:/root/.aws ansiblecm \
+    -v ${HOME}/.aws:/root/.aws \
     --workdir $(pwd) \
+    --env USER=${USER} \
+    ansiblecm:2.6.4 \
     -i /ansible/playbooks/hosts \
     /ansible/playbooks/playbook.yml
