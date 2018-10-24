137be0ba219bfc7b5d5131efb892fb173de72dde
Ronald Dilley <rdilley@contractor.usgs.gov>
2018-10-24 14:10:07 +0000

    Updated to match the documentation example and latest versions of the queue and job definitions

--- ips-local-testing/run_multi.sh
+++ ips-local-testing/run_multi.sh
@@ -1,12 +1,12 @@
 #!/bin/bash +x
 
-export IPS_ALL_DIR=/devel/djt/ips-all
-export IPS_QUEUE=djt-batch-IPS_ProcessJobQueueSpot
-export IPS_JOBDEF=djt-batch-IPS_ProcessJob
+export IPS_ALL_DIR=/devel/<YOUR-DEVEL-PATH>/ips-all
+export IPS_QUEUE=ips2-devel-batch-ProcessJobQueueSpot
+export IPS_JOBDEF=ips2-devel-batch-IPS_ProcessJob
 
 $IPS_ALL_DIR/espa-container-tools/ips-local-testing/ips_submit_multi.py \
                --queue $IPS_QUEUE \
                --job-definition $IPS_JOBDEF \
-               --input-bucket dev-lsds-l8-test-l0rp \
-               --job-bucket jdc-test-dev \
+               --input-bucket usgs-landsat \
+               --prefix data/scene/l0rp/L08 \
                $1 
