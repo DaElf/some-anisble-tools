166a1bfa1622096bc349fdb7325d4c0cecacd62d
Ethan Post <ethan.post.ctr@usgs.gov>
2019-01-31 22:14:51 +0000

    having Jenkins update the hosts file with the new ip.

--- ec2_tools/ansible/update-hosts-file.sh
+++ ec2_tools/ansible/update-hosts-file.sh
@@ -1,3 +1,3 @@
-echo "aws-devel-instance ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_ssh_host=$(aws ec2 describe-instances --filter "Name=instance-state-name,Values=running,Name=tag:Name,Values=epost-devel" --query "Reservations[].Instances[].[PublicIpAddress]" --output text)"
+echo "aws-devel-instance ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_ssh_host=$(aws ec2 describe-instances --filter "Name=instance-state-name,Values=running,Name=tag:Name,Values=$USER-devel" --query "Reservations[].Instances[].[PublicIpAddress]" --output text)" > hosts
 
 echo "jenkins-docker-worker ansible_ssh_common_args='-o StrictHostKeyChecking=no' ansible_ssh_host=$(aws ec2 describe-instances --filter "Name=instance-state-name,Values=running,Name=tag:Name,Values=dilley-dev-spot-host" --query "Reservations[].Instances[].[PublicDnsName]" --output text)"
